98ea3364ed74fc41d1938863eea6299749f82109
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-21 14:17:30 +0000

    my first web page with PHP.

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-    <meta charset="utf-8">
-	<meta name="description" content="My first webpage, with JavaScript">
-    <meta name="keywords" content="immaculata, ics2o">
-    <meta name="author" content="Kukwac">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="./Favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./Favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./Favicons/favicon-16x16.png">
-    <link rel="manifest" href="./Favicons/site.webmanifest">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Hello, World! in PHP</title>
-	</head>
-	<body>
-    <h1><?php echo 'Hello, World! in PHP'; ?></h1>
-		<?php echo '<p>This is my first web page using PHP.</p>'; ?>
-	</body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="My first webpage, with JavaScript">
+        <meta name="keywords" content="immaculata, ics2o">
+        <meta name="author" content="Kukwac">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="apple-touch-icon" sizes="180x180" href="./Favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./Favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./Favicons/favicon-16x16.png">
+        <link rel="manifest" href="./Favicons/site.webmanifest">
+        <link rel="stylesheet" href="./css/style.css">
+        <title>Hello, World! in PHP</title>
+    </head>
+    <body>
+        <h1><?php echo 'Hello, World! in PHP'; ?></h1>
+        <?php echo '<p>This is my first web page, using PHP.</p>'; ?>
+    </body>
 </html>
