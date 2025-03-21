b86c5da42ff20d7d9509e389b598cdc1a07819dc
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-21 14:27:33 +0000

    commented out favicons

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
         <meta name="keywords" content="immaculata, ics2o">
         <meta name="author" content="Kukwac">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- 
         <link rel="apple-touch-icon" sizes="180x180" href="./Favicons/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="./Favicons/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="./Favicons/favicon-16x16.png">
-        <link rel="manifest" href="./Favicons/site.webmanifest">
+        <link rel="manifest" href="./Favicons/site.webmanifest"> 
+        -->
         <title>Hello, World! in PHP</title>
     </head>
     <body>
