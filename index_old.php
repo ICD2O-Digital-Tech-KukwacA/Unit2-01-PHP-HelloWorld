d5089cff3c10332e4d011001a299a96034471906
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-21 14:35:27 +0000

    using double quotes

---
+++ index_old.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="My first webpage, with JavaScript">
+        <meta name="keywords" content="immaculata, ics2o">
+        <title>Hello, World! in PHP</title>
+    </head>
+    <body>
+        <h1><?php echo "Hello, World! in PHP"; ?></h1>
+        <p><?php echo "This is my first web page, using PHP."; ?></p>
+    </body>
+</html>
