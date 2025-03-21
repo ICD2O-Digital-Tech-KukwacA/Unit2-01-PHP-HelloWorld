479dc1d2745545448f733eb08953763b52abe17b
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-21 14:38:13 +0000

    using double quotes

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <meta charset="uft-8">
+    <meta charset="UTF-8">
     <title>Hello World</title>
   </head>
   <body>
-    <?php echo "<p>My name is Kukwac and I am a grade 10 student.</p>"; ?>
+    <?php echo '<p>My name is Kukwac and I am a grade 10 student.</p>'; ?>
   </body>
 </html>
