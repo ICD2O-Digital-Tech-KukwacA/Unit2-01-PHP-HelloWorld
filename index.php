707cab5534567496b72e1118b962027c00d7fc11
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-21 14:29:54 +0000

    moved p tag

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="description" content="My first webpage, with JavaScript">
         <meta name="keywords" content="immaculata, ics2o">
-        <meta name="author" content="Kukwac">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- <meta name="author" content="Kukwac">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
         <!-- 
         <link rel="apple-touch-icon" sizes="180x180" href="./Favicons/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="./Favicons/favicon-32x32.png">
@@ -16,6 +16,6 @@
     </head>
     <body>
         <h1><?php echo 'Hello, World! in PHP'; ?></h1>
-        <?php echo '<p>This is my first web page, using PHP.</p>'; ?>
+        <p><?php echo 'This is my first web page, using PHP.'; ?></p>
     </body>
 </html>
