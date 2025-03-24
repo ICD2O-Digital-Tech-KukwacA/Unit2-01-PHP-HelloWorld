d4b1eb4e772038da60fbd6b316fd67d2de0a3775
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-24 14:01:39 +0000

    Updated version of my first web pagfe with PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE PHP>
+<html lang="en">
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="./Favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./Favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./Favicons/favicon-16x16.png">
+    <link rel="manifest" href="./Favicons/site.webmanifest">
+    <title>Hello, World! in PHP</title>
+	</head>
+	<body>
+    <h1><?php echo 'Hello, World! in PHP'; ?></h1>
+		<?php echo '<p>This is my first web page using PHP.</p>'; ?>
+	</body>
+</html>
